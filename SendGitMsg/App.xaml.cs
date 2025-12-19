9b84cc64ebec6f7bbba0a85a0cf57ff64444bc89
Nayoon <kny99306ny@gmail.com>
2025-12-19 13:24:28 +0900

    초기 커밋.

---
+++ SendGitMsg/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SendGitMsg
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
