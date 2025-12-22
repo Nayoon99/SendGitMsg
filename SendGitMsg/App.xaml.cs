20cc4d99e77196310c1904e4456f3ad72b18def4
Nayoon <kny99306ny@gmail.com>
2025-12-22 15:23:02 +0900

    [Design] UI 변경

--- SendGitMsg/App.xaml.cs
+++ SendGitMsg/App.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using SendGitMsg.sqlite;
 
 namespace SendGitMsg
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DatabaseInitializer.Init();
+        }
     }
 
 }
--- SendGitMsg/ViewModel/MainViewModel.cs
+++ SendGitMsg/ViewModel/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Windows.Threading;
 using System.Diagnostics;
 using System.Text;
+using SendGitMsg.sqlite;
 
 namespace SendGitMsg.ViewModel
 {
@@ -23,8 +24,23 @@ namespace SendGitMsg.ViewModel
         // 생성자
         public MainViewModel()
         {
+            StatusMsg = "ViewModel 로딩됨";
+
             // 버튼 커맨드
             CheckCommitCommand = new Command(CheckCommits);
+
+            LoadCommitLogs();
+
+            if (CommitLog.IsTodayAlreadyExecuted())
+            {
+                StatusMsg = "오늘은 이미 커밋 체크를 완료했습니다.";
+                CanCheckCommit = false;
+            }
+            else
+            {
+                StatusMsg = "아직 커밋 체크를 하지 않았습니다.";
+                CanCheckCommit = true;
+            }
 
             // 타이머 생성
             _timer = new DispatcherTimer();
@@ -34,19 +50,42 @@ namespace SendGitMsg.ViewModel
         }
 
 
-        // 커밋 체크 + 메시지 전송 로직
+        // 커밋 체크 + 메시지 전송 로직 메서드
         private void CheckCommits()
         {
-            int commitCount = GetTodayCommitCount();
+            // 오늘 이미 실행했으면 중단
+            if (CommitLog.IsTodayAlreadyExecuted())
+            {
+                StatusMsg = "오늘은 이미 커밋 체크를 완료했습니다.";
+                CanCheckCommit = false;
+                return;
+            }
 
+            int commitCount = GetYesterdayCommitCount();
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            string smsStatus = commitCount > 0 ? "전송 예정" : "어제 커밋이 없습니다!";
+            string slackStatus = "대기";
+
+            // DB INSERT
+            CommitLog.InsertCommitLog(
+                today,
+                commitCount,
+                smsStatus,
+                slackStatus);
+
+            // 화면 표시
             Records.Add(new CommitRecord
             {
-                Date = DateTime.Now.ToString("yyyy-MM-dd"),
+                Date = today,
                 CommitCnt = commitCount,
-                SmsStts = "대기",
-                SlackStts = "대기"
+                SmsStts = smsStatus,
+                SlackStts = slackStatus
             });
+
+            StatusMsg = "커밋 체크가 완료되었습니다.";
+            CanCheckCommit = false;
         }
+
 
 
 
@@ -74,12 +113,16 @@ namespace SendGitMsg.ViewModel
         }
 
 
-        private int GetTodayCommitCount()
+        // 전날 커밋 개수 카운트 메서드
+        private int GetYesterdayCommitCount()
         {
+            DateTime today = DateTime.Today;
+            DateTime yesterday = today.AddDays(-1);
+
             var startInfo = new ProcessStartInfo
             {
                 FileName = "git",
-                Arguments = "log --since=midnight --oneline",
+                Arguments = $"log --since=\"{yesterday:yyyy-MM-dd} 00:00\" --until=\"{today:yyyy-MM-dd} 00:00\" --oneline",
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
@@ -101,6 +144,33 @@ namespace SendGitMsg.ViewModel
         }
 
 
+        private string _statusMsg;
+        public string StatusMsg
+        {
+            get => _statusMsg;
+            set => SetProperty(ref _statusMsg, value);
+        }
+
+        private bool _canCheckCommit = true;
+        public bool CanCheckCommit
+        {
+            get => _canCheckCommit;
+            set => SetProperty(ref _canCheckCommit, value);
+        }
+
+
+
+        private void LoadCommitLogs()
+        {
+            Records.Clear();
+
+            var logs = CommitLog.GetAllLogs();
+
+            foreach (var log in logs)
+            {
+                Records.Add(log);
+            }
+        }
 
     }
 }
